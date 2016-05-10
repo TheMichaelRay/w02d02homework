b6e5f2bd2c0109e7c74847848338704e10cf2f5d
Michael Ray <raymichael23@gmail.com>
2016-05-10 16:38:43 -0700

    organized cal object and fixed event listeners for button testers

--- app.js
+++ app.js
@@ -1,18 +1,28 @@
 var cal = {
+
+  //DOM selectors
   buttons: {
     numbers: document.querySelectorAll('.num'),
     operators: document.querySelectorAll('.oper'),
-    equals: document.querySelector('.equal')
+    equals: document.querySelector('#equals')
     },
-  display: document.querySelector('#display')
-}
+  display: document.querySelector('#display'),
 
-function addNum(){
-  console.log(Number(this.innerHTML))
+  //functions
+  operations: {
+    addNum: function(){console.log(Number(this.innerHTML))},
+    addOp: function(){console.log(this.innerHTML)},
+    sum: function(a,b) {return a+b},
+    diff: function(a,b) {return a-b},
+    prod: function(a,b) {return a*b},
+    quot: function(a,b) {return a/b}
+    }
 }
 
 for (var i = 0; i < cal.buttons.numbers.length; i++) {
-  cal.buttons.numbers[i].addEventListener('click', function(){
-    console.log(Number(this.innerHTML));
-  })
-}
+  cal.buttons.numbers[i].addEventListener('click', cal.operations.addNum)
+};
+for (var i = 0; i < cal.buttons.operators.length; i++) {
+  cal.buttons.operators[i].addEventListener('click', cal.operations.addOp)
+};
+cal.buttons.equals.addEventListener('click', cal.operations.addOp)

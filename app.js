21dc62ce19cd1513171647237568b762ef73dccd
Michael Ray <raymichael23@gmail.com>
2016-05-10 15:14:14 -0700

    added event listeners to each button for testers

---
+++ app.js
@@ -0,0 +1,7 @@
+var buttons = document.querySelectorAll('button');
+
+for (var i = 0; i < buttons.length; i++) {
+  buttons[i].addEventListener('click', function(){
+    console.log(Number(this.innerHTML));
+  })
+}

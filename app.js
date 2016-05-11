98b1326542e8e1bbf9e1c28c3e67c6121fbe2c29
Michael Ray <raymichael23@gmail.com>
2016-05-10 23:15:49 -0700

    added comments and spacing for js code

--- app.js
+++ app.js
@@ -2,41 +2,62 @@ var cal = {
 
   //DOM selectors
   buttons: {
+
     numbers: document.querySelectorAll('.num'),
+
     operators: document.querySelectorAll('.oper'),
+
     equals: document.querySelector('#equals'),
+
     clear: document.querySelector('#clear')
     },
+
+  //Calculator display
   display: document.querySelector('#display'),
 
   //functions
   operations: {
     clear: function(){cal.display.value = "";
-                      cal.display.placeholder = "";
+                      cal.display.placeholder = "0";
                       cal.hold.arr = []},
+
     addNum: function(){cal.display.value += this.innerHTML},
+
+                        //used if the user is using multiple numbers
+                        //and operators to continue ad infinitum
     addOp: function(){if (cal.hold.arr.length == 2){
                           cal.hold.arr.push(cal.display.value);
-                          cal.display.value = eval(cal.hold.arr.join(''));
-                          cal.hold.arr = []}
+                          cal.display.value = cal.operations.calc(cal.hold.arr[0], cal.hold.arr[1], cal.hold.arr[2]);
+                          cal.hold.arr = [];
+                        //used if the user presses = and then wants to
+                        //continue operations
+                      } else if (Number(cal.display.placeholder) > 0) {
+                          cal.hold.arr.push(cal.display.placeholder);
+                          cal.hold.arr.push(this.innerHTML)
+                      } else {
                       cal.hold.arr.push(cal.display.value);
                       cal.display.placeholder = cal.display.value;
                       cal.hold.arr.push(this.innerHTML);
-                      cal.display.value = ""},
+                      cal.display.value = ""}},
+
     equals: function(){cal.hold.arr.push(cal.display.value);
-                       cal.display.placeholder = cal.operations.calc(cal.hold.arr);
+                       cal.display.placeholder = cal.operations.calc(cal.hold.arr[0], cal.hold.arr[1], cal.hold.arr[2]);
                        cal.display.value = '';
                        cal.hold.arr = []},
+
+          // takes the cal.hold.arr array and returns a numerical value
     calc: function(num1, operator, num2) {
           if (operator === '+') {
-            return cal.operations.sum(num1, num2);
+            return cal.operations.sum(Number(num1), Number(num2));
           } else if (operator === '-') {
-            return cal.operations.diff(num1, num2);
+            return cal.operations.diff(Number(num1), Number(num2));
           } else if (operator === '*') {
-            return cal.operations.prod(num1, num2);
+            return cal.operations.prod(Number(num1), Number(num2));
           } else if (operator === '/') {
-            return cal.operations.quot(num1, num2);
+            return cal.operations.quot(Number(num1), Number(num2));
           } else {alert('ugh')}},
+
+          //arithemical functions
     sum: function(a,b) {return a+b},
     diff: function(a,b) {return a-b},
     prod: function(a,b) {return a*b},
@@ -45,7 +66,9 @@ var cal = {
 
   //holding zone variables
   hold: {
-    arr: []
+    arr: [],
+    // mem used for future memory store and recall function
+    mem: null
   }
 }
 
